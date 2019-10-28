84bd2c9908511de3990085677e86e2b8718008fc
anton1x <anton1x46@gmail.com>
2019-10-29 02:26:45 +0300

    Basic Symfony skeleton with SonataAdminBundle integration

---
+++ src/Admin/AddressAdmin.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anton1x
+ * Date: 28.10.2019
+ * Time: 19:30
+ */
+
+namespace App\Admin;
+
+
+use App\Entity\City;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Form\Type\ModelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class AddressAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('title', TextType::class);
+        $formMapper->add('description', TextareaType::class);
+        $formMapper->add('city', ModelType::class, [
+            'class' => City::class,
+            'property' => 'title',
+        ]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('title');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('title');
+    }
+}
